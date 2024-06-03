0cea42f09b92edd19457a345b9b1978841b3af0c
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2024-06-03 14:36:45 +0200

    added margin for mobile

--- template-parts/frontPageUSP.php
+++ template-parts/frontPageUSP.php
@@ -21,22 +21,17 @@
             <div class="slide">
                 <img src="<?php echo get_theme_file_uri( 'assets/images/FrontPageStepOne.svg' ); ?>" alt="">
                 <h2>Convenient</h2>
-
-                    <p>Cohabit delivers your furniture into your apartment</p>
-
+                <p>Cohabit delivers your furniture into your apartment</p>
             </div>
             <div class="slide">
                 <img src="<?php echo get_theme_file_uri( 'assets/images/FrontPageStepTwo.svg' ); ?>" alt="">
                 <h2>Affordable</h2>
-
                 <p>Our pre-loved packages start at <span class="boldPrice">299 sek/month</span></p>
-
             </div>
             <div class="slide">
                 <img src="<?php echo get_theme_file_uri( 'assets/images/FrontPageStepThree.svg' ); ?>" alt="">
                 <h2>Sustainable</h2>
-                    <p>Renting pre-loved furniture is more planet-friendly</p>  
-                
+                <p>Renting pre-loved furniture is more planet-friendly</p>
             </div>
 
     </div>
