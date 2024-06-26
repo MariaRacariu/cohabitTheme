0d19171be7a86dad74c6922e78b8b02acdef559a
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2024-06-26 12:51:30 +0200

    fixed bugs

--- page-contact.php
+++ page-contact.php
@@ -8,11 +8,11 @@ get_header();
 <div>
     <div class="mainContainer">
         <div class="contactFormContainer">
+            <h2 class="introHeading">Get in touch with us</h2>
+            <p>amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
             <?php echo do_shortcode('[wpforms id="90"]'); ?>
         </div>
         <div class="contactInfoContainer">
-            <h2 class="introHeading">Get in touch with us</h2>
-            <p>amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut</p>
             <div class="linksContainer">
                 <div>
                     <a href="mailto:hello@cohabit.se" class="formLinks"><i class="fa-solid fa-envelope fa-xl"></i><p class="linkText secondFont">hello@cohabit.se</p></a>
