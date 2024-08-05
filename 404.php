53dddb131f2d3c18f588f44739fd8726ebd70953
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2024-08-05 13:53:39 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="notFoundContainer">
+    <h1>404 Page not found</h1>
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/undraw_page_not_found.svg'; ?>" alt="404 page not found">
+    <p>Please come back later, we are having some technical issues</p>
+</div>
